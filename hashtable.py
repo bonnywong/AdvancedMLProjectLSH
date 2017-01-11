6add7514825448c1b5de3ecdb4d2c07daadc4000
niklasrolleberg <niklasr93@gmail.com>
2017-01-11 12:02:03 +0100

    lite nytt

--- hashtable.py
+++ hashtable.py
@@ -3,15 +3,14 @@ import numpy as np
 
 class HashTable:
 
-    def __init__(self, size,maxb,maxp):
+    def __init__(self,size,k,maxb,maxp):
         print("Init hashtable")
-        dims = 4
         self.M = size
         self.C = maxp
         self.bucketSize = maxb
 
-        self.hash_index = np.random.randint(maxp,size=5)
-        self.hash_const = np.random.randint(self.M-1,size=5)
+        self.hash_index = np.random.randint(maxp,size=k)
+        self.hash_const = np.random.randint(self.M-1,size=k)
         self.table = np.empty((self.M), dtype=object)
         for i in range(self.M):
             self.table[i] = []
@@ -27,18 +26,24 @@ class HashTable:
     def add_point(self,point):
         index = self.LSH(point)
         if (len(self.table[index]) < self.bucketSize):
-            self.table[self.LSH(point)].append(point)
-        print(self.table)
+            self.table[index].append(point)
+        else:
+            for i in range(1,self.M):
+                index=index+1
+                if (len(self.table[(index)%self.M]) < self.bucketSize):
+                    self.table[(index)%self.M].append(point)
+                    return
 
     def get_bucket(self,point):
         return self.table[self.LSH(point)]
 
 
-# Hashtable(M,B,C)
+# Hashtable(M,k,B,C)
 # M = size of hashtalbe
+# k = dimensions for g and a (number of bits to use from unary array)
 # B = max size of buckets
 # C = largest number in dataset
-H = HashTable(10,2,2)
+H = HashTable(10,5,1,2)
 
 point = np.array([1,2,1])
 point2 = np.array([2,1,2])
