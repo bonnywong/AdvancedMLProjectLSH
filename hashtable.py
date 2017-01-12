686a34698cf6f1cf37b1b1ca211549098d4ec4ad
niklasrolleberg <niklasr93@gmail.com>
2017-01-12 13:17:41 +0100

    hej

--- hashtable.py
+++ hashtable.py
@@ -3,14 +3,17 @@ import numpy as np
 
 class HashTable:
 
-    def __init__(self,size,k,maxb,maxp):
+    def __init__(self,size,k,dim,maxb,maxp):
         print("Init hashtable")
         self.M = size
         self.C = maxp
         self.bucketSize = maxb
 
-        self.hash_index = np.random.randint(maxp,size=k)
+        self.hash_index = np.random.randint(self.C*dim,size=k)
         self.hash_const = np.random.randint(self.M-1,size=k)
+        #print(self.hash_const)
+        #self.hash_const.tofile('foo.csv',sep=',',format='%10.5f')
+        #np.random.randint(self.M-1,size=k)
         self.table = np.empty((self.M), dtype=object)
         for i in range(self.M):
             self.table[i] = []
@@ -19,15 +22,17 @@ class HashTable:
         """Hash function without converting to unary"""
         h = 0;
         for i in range(point.size):
-            if(self.hash_index[i]%self.C < point[self.hash_index[i]//self.C]):
+            if(self.hash_index[i]%self.C <= point[self.hash_index[i]//self.C]):
                 h += self.hash_const[i]
         return h % self.M
 
     def add_point(self,point):
         index = self.LSH(point)
+        #print("Hash: " + str(index))
         if (len(self.table[index]) < self.bucketSize):
             self.table[index].append(point)
         else:
+            #print("Bucket full: hash="+str(index) + " point[0]=" + str(point[0]))
             for i in range(1,self.M):
                 index=index+1
                 if (len(self.table[(index)%self.M]) < self.bucketSize):
@@ -37,10 +42,11 @@ class HashTable:
     def get_bucket(self,point):
         return self.table[self.LSH(point)]
 
-
-# Hashtable(M,k,B,C)
+'''
+# Hashtable(M,k,dim,B,C)
 # M = size of hashtalbe
 # k = dimensions for g and a (number of bits to use from unary array)
+# dim = dimensions of the data
 # B = max size of buckets
 # C = largest number in dataset
 H = HashTable(10,5,1,2)
@@ -57,3 +63,4 @@ print(H.get_bucket(point))
 
 print('hash')
 print(H.LSH(point))
+'''
